7acbd882c69299844944e9cecf6f006e64ceb043
mba-hung-tm <huy.tran.quoc@valuence.inc>
2021-07-07 17:51:43 +0700

    init app & show tasks

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,34 @@
+import {Task} from './Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Doctors Appointment',
+    day: 'May 5th at 2:30 pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Doctors Appointment 1',
+    day: 'May 5th at 2:30 pm',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Doctors Appointment 2',
+    day: 'May 5th at 2:30 pm',
+    reminder: true,
+  },
+  {
+    id: 4,
+    text: 'Doctors Appointment 3',
+    day: 'May 5th at 2:30 pm',
+    reminder: true,
+  },
+  {
+    id: 5,
+    text: 'Doctors Appointment 4',
+    day: 'May 5th at 2:30 pm',
+    reminder: true,
+  },
+];
